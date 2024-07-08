13e793e50fbb48bf15181019b23beb36a0835dcb
ejmockler <emockler@ucsc.edu>
2024-07-07 19:22:27 -0700

    sync sveltekit code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+	import aeonLogo from '$lib/images/aeon.svg';
+	import '@fontsource/montserrat';
+</script>
+
+<svelte:head>
+	<title>Aeon</title>
+	<meta name="description" content="Aeon Biosciences Welcome Page" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={aeonLogo} type="image/svg" />
+				<img src={aeonLogo} alt="Aeon" />
+			</picture>
+		</span>
+
+		Aeon Biosciences
+	</h1>
+
+	<h2>
+		Mapping biomolecules beyond foundation models,
+		<br />
+		for the future of medicine & longevity.
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1,
+	h2 {
+		width: 100%;
+		font-family: 'Montserrat', sans-serif;
+		color: whitesmoke;
+		text-align: center;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
